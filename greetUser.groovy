c7296613db6d28a9c78e28d2c5eb288a07703ea2
Bharathtemenos <52853553+Bharathtemenos@users.noreply.github.com>
2024-04-04 15:41:19 +0200

    Create greetUser.groovy

---
+++ greetUser.groovy
@@ -0,0 +1,7 @@
+// Define a function to greet a user
+def greetUser(String username) {
+    println "Hello, $username! Welcome to Jenkins Groovy scripting."
+}
+
+// Call the function to greet a user
+greetUser("John")
