2b67f86e4299b983986fa33950f78b31e38dec9b
Bharathtemenos <52853553+Bharathtemenos@users.noreply.github.com>
2024-04-05 14:30:51 +0200

    Update t24test.groovy

--- vars/t24test.groovy
+++ vars/t24test.groovy
@@ -1,10 +1,10 @@
-pipeline {
-    agent any
-    stages {
-        stage('Hello') {
-            steps {
-                echo 'Hello World'
-            }
+def call(body) {
+    // Delegate processing of the DSL block to another method
+    pipeline {
+        agent any
+        stages {
+            // Execute the provided closure (body) within the pipeline block
+            body()
         }
     }
 }
